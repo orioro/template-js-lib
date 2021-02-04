cddaa794e8c69aef5c918e0dd4fef97c9aa22b61
Simon Fan <simon@orioro.design>
2021-02-04 00:32:36 -0300

    feat(docs): add built in support for @orioro/readme generators

---
+++ readme.md
@@ -0,0 +1,13 @@
+# projectName
+
+# API Docs
+
+- src/index.ts
+  - [`projectName(param)`](#projectnameparam)
+
+##### `projectName(param)`
+
+projectName description
+
+- `param` {string}
+- Returns: `result` {string}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
+/**
+ * projectName description
+ *
+ * @todo scope-1 Something to be done
+ * @todo scope-2 Something else to be done
+ * 
+ * @function projectName
+ * @param {string} param
+ * @returns {string} result
+ */
 export const projectName = (param: string):string => `output "${param}"`
