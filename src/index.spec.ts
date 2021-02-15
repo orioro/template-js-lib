874824185781563bea4846b4d4b47cb573b08611
Simon Fan <simon@orioro.design>
2021-02-15 18:37:28 -0300

    feat: add code linting automation workflow

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,7 +1,7 @@
 import { projectName } from './index'
 
 describe('tests', () => {
-  test('test', () => {
+  test('test-1', () => {
     expect(projectName('parameter')).toEqual('output "parameter"')
   })
 })
--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,9 @@
  *
  * @todo scope-1 Something to be done
  * @todo scope-2 Something else to be done
- * 
+ *
  * @function projectName
  * @param {string} param
  * @returns {string} result
  */
-export const projectName = (param: string):string => `output "${param}"`
+export const projectName = (param: string): string => `output "${param}"`
